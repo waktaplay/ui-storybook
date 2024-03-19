27a6c04fd13335c6c068adc029199d869c9fb746
guejukmejeok <hantecude123@gmail.com>
2024-03-19 13:22:17 +0900

    Feat: Navigationbar.tsx 추가

---
+++ src/stories/NavigationBar.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface NavigationBarProps {
+    // Define your props here
+}
+
+const NavigationBar: React.FC<NavigationBarProps> = ({ /* Destructure your props here */ }) => {
+    return (
+        <nav>
+            {/* Your navigation bar content goes here */}
+        </nav>
+    );
+};
+
+export default NavigationBar;
